5048634990c04eeefa9ed643bd5be4374dab1086
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-26 08:44:43 +0600

    added some dynamic data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
      <Header></Header>
+     {/* <Shop></Shop> */}
+     <Shop></Shop>
+     
     </div>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products,setProducts] = useState(first10);
+    return (
+        <div className ="shop-container">
+           <div className="product-container">
+           
+                {
+                    products.map(pd => <Product product = {pd}></Product> )
+                }
+            
+           </div>
+            {/* <h3>{products.length}</h3> */}
+         <div className="cart-container">
+             <h3>This is cart container</h3>
+         </div>
+        </div>
+    );
+};
+
+export default Shop;
